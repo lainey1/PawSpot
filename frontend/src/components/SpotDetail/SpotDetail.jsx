dd840e1c22933dd1086a81273c6bf05d0c2d67cd
Lainey <misslenlen@gmail.com>
2024-10-17 10:52:17 -0700

    reserve alert and styling updated

--- frontend/src/components/SpotDetail/SpotDetail.jsx
+++ frontend/src/components/SpotDetail/SpotDetail.jsx
@@ -1,10 +1,11 @@
 // frontend/src/components/SpotDetail.jsx
 
 // #1 IMPORT necessary hooks and components
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getSpots } from "../../store/spots";
+import "./SpotDetail.css";
 
 // #2 DEFINE COMPONENT
 const SpotDetail = () => {
@@ -14,26 +15,41 @@ const SpotDetail = () => {
   const spots = useSelector((state) => state.spot.list);
   const spot = spots.find((spot) => spot.id === Number(spotId));
 
+  // Replace the traditional alert with a custom alert component
+  const [showAlert, setShowAlert] = useState(false);
+
   useEffect(() => {
     dispatch(getSpots());
   }, [dispatch]);
 
   if (!spot) return <div>Loading...</div>;
 
+  //* CLICK HANDLER for the Reserve button
+
+  const handleReserveClick = () => {
+    setShowAlert(true);
+    setTimeout(() => setShowAlert(false), 3000);
+  };
+
   // #3 RENDER METHOD
   return (
     <div className="spot-detail-container">
       <div className="spot-detail">
-        <h1>{spot.title}</h1>
-        <h2>
+        <h2>{spot.name}</h2>
+        <p className="spot-location">
           {spot.city}, {spot.state}, {spot.country}
-        </h2>
+        </p>
         <p>{spot.description}</p>
       </div>
 
       <div className="bookit-sidebar">
         <p className="price">${spot.price} per night</p>
-        <button className="reserve-button">Reserve</button>
+        <button className="reserve-button" onClick={handleReserveClick}>
+          Reserve
+        </button>
+        {showAlert && (
+          <span className="alert-message">Feature Coming Soon...</span>
+        )}
       </div>
     </div>
   );
