e488eeb7deab01a6fcdb1363f9ff7a03df3523e9
lainey1 <lainey.d.paddock@gmail.com>
2024-11-24 01:12:29 -0800

    fixed dom els

--- frontend/src/components/SpotsList/SpotsList.jsx
+++ frontend/src/components/SpotsList/SpotsList.jsx
@@ -1,7 +1,27 @@
 // frontend/src/components/SpotsList/SpotsList.jsx
 
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getSpots } from "../../store/spots";
+
 const SpotsList = () => {
-  return <h1>Spot List</h1>;
+  const dispatch = useDispatch();
+
+  const spots = useSelector((state) => state.spot);
+  const spotsList = Object.values(spots);
+  console.log(spotsList);
+
+  useEffect(() => {
+    dispatch(getSpots());
+  }, [dispatch]);
+  return (
+    <div>
+      <h1>Spot List</h1>
+      {spotsList?.map(({ id, name }) => (
+        <p key={id}>{name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default SpotsList;
