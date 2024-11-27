31f53738863b2db37eee5ee8033df7b8cffd86fa
lainey1 <lainey.d.paddock@gmail.com>
2024-11-26 19:01:04 -0800

    roll back

--- frontend/src/components/SpotDetail/SpotDetail.jsx
+++ frontend/src/components/SpotDetail/SpotDetail.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import { fetchSpot } from "../../store/spots";
 import { fetchReviews } from "../../store/reviews";
 import Reviews from "../SpotReviews";
 import "./SpotDetail.css";
