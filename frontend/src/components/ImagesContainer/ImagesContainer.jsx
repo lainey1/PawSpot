1988047495ec62461a78930199b4529fed5fe96e
lainey1 <lainey.d.paddock@gmail.com>
2024-11-28 02:29:37 -0800

    image viewer

--- frontend/src/components/ImagesContainer/ImagesContainer.jsx
+++ frontend/src/components/ImagesContainer/ImagesContainer.jsx
@@ -1,30 +1,13 @@
 import { useState } from "react";
+import ImageViewer from "../ImageViewer";
 import "./ImagesContainer.css";
+import "../ImageViewer/ImageViewer.css";
 
 const ImagesContainer = ({ spot }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const openModal = (index) => {
-    setCurrentIndex(index);
+  const openModal = () => {
     setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setCurrentIndex(0);
-  };
-
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? spot.SpotImages.length - 1 : prevIndex - 1
-    );
-  };
-
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === spot.SpotImages.length - 1 ? 0 : prevIndex + 1
-    );
   };
 
   return (
@@ -64,26 +47,7 @@ const ImagesContainer = ({ spot }) => {
         </div>
       )}
 
-      {isModalOpen && (
-        <div className="image-viewer-modal" onClick={closeModal}>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <button className="nav-button prev-button" onClick={goToPrevious}>
-              &#8249;
-            </button>
-            <img
-              src={spot.SpotImages[currentIndex]?.url}
-              alt={`Image ${currentIndex + 1}`}
-              className="modal-image"
-            />
-            <button className="nav-button next-button" onClick={goToNext}>
-              &#8250;
-            </button>
-            <button className="close-button" onClick={closeModal}>
-              &times;
-            </button>
-          </div>
-        </div>
-      )}
+      {isModalOpen && <ImageViewer values={{ spot, setIsModalOpen }} />}
     </div>
   );
 };
