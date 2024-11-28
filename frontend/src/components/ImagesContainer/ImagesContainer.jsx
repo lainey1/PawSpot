c1d3f4ae94a40b714d249a4d224210e48a05a42f
lainey1 <lainey.d.paddock@gmail.com>
2024-11-27 23:19:02 -0800

    stylin scrub

--- frontend/src/components/ImagesContainer/ImagesContainer.jsx
+++ frontend/src/components/ImagesContainer/ImagesContainer.jsx
@@ -1,36 +1,38 @@
 import "./ImagesContainer.css";
 
 const ImagesContainer = ({ spot }) => (
-  <div className="images-container">
-    <div className="image-large-container">
+  <div id="images-container">
+    <div id="image-large-container">
       <img
         className="image-large"
         src={spot.SpotImages[0]?.url}
         alt={spot.name}
       />
     </div>
-    <div className="images-small-container">
-      <div className="images-small-row">
-        {spot.SpotImages.slice(1, 3).map((image, index) => (
-          <img
-            key={index}
-            src={image?.url}
-            className="image-small"
-            alt={`Small Image ${index + 1}`}
-          />
-        ))}
+    {spot.SpotImages.length >= 5 && (
+      <div id="images-small-container">
+        <div className="images-small-row">
+          {spot.SpotImages.slice(1, 3).map((image, index) => (
+            <img
+              key={index}
+              src={image?.url}
+              className="image-small"
+              alt={`Small Image ${index + 1}`}
+            />
+          ))}
+        </div>
+        <div className="images-small-row">
+          {spot.SpotImages.slice(3, 5).map((image, index) => (
+            <img
+              key={index}
+              src={image?.url}
+              className="image-small"
+              alt={`Small Image ${index + 3}`}
+            />
+          ))}
+        </div>
       </div>
-      <div className="images-small-row">
-        {spot.SpotImages.slice(3, 5).map((image, index) => (
-          <img
-            key={index}
-            src={image?.url}
-            className="image-small"
-            alt={`Small Image ${index + 3}`}
-          />
-        ))}
-      </div>
-    </div>
+    )}
   </div>
 );
 
